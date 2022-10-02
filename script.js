fb55fc6042bd3e8f5fb8ce7ff95ca98486a8ffab
michael <michaelmadlos@hotmail.com>
2022-10-02 13:04:41 +0200

    Add buttons and events

--- script.js
+++ script.js
@@ -1,62 +1,59 @@
-// The computer selects randomly: rock, paper o scissors.
+function game() {
+    // Computer selection
 
-let options = ["Rock", "Paper", "Scissors"];
+    function getComputerChoice(){
+        let options = ["Rock", "Paper", "Scissors"];
+        let optionsLength = options.length;
 
-function getComputerChoice(){
-    let optionsLength = options.length;
-    let randomIndex = () => Math.floor(Math.random() * optionsLength);
+        let randomIndex = () => Math.floor(Math.random() * optionsLength);
+        return computerChoice = options[randomIndex()];
+    }
 
-    return computerChoice = options[randomIndex()];
+    // Player selection
+
+    const div = document.querySelector("div");
+    const body = document.querySelector("body");
+    const buttons = document.querySelectorAll("button");
+
+
+    buttons.forEach((button) => {
+        
+        button.addEventListener("click", () => {
+            const playerSelection = button.textContent;
+            let computerSelection = getComputerChoice();
+            console.log(playerSelection);   
+            
+            playRound(playerSelection, computerSelection);
+        })        
+    })
+
+    function playRound(playerChoice, computerSelection) {
+        
+        if (playerChoice == "Rock" && computerSelection == "Scissors"){
+            div.textContent = `You have selected ${playerChoice} against ${computerSelection}. YOU WON!!`
+            body.appendChild(div);
+        } else if (playerChoice == "Paper" && computerSelection == "Rock"){
+            div.textContent = `You have selected ${playerChoice} against ${computerSelection}. YOU WON!!`
+            body.appendChild(div);
+        } else if (playerChoice == "Scissors" && computerSelection == "Paper"){
+            div.textContent = `You have selected ${playerChoice} against ${computerSelection}. YOU WON!!`
+            body.appendChild(div);
+        } else if (playerChoice == computerSelection) {
+            div.textContent = `You have selected ${playerChoice} against ${computerSelection}. TIE!!`
+            body.appendChild(div);        
+        }            
+        else {
+            div.textContent = `You have selected ${playerChoice} against ${computerSelection} YOU LOSE! `;
+            body.appendChild(div);
+        } 
+    };
+
+    
 }
 
-let playerScore = 0;
-let computerScore = 0;
-const messageWin = "You win!";
-const messageTie = "Tie!";
-const messageLose = "You lose!";
 
-function game(){
 
-    // Indica resultado de la partida y suma los puntos
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection == "Rock" && computerSelection == "Scissors") {
-            playerScore++;
-            return `${messageWin} Your score is ${playerScore}`;
+// Indica resultado de la partida
 
-        } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-            playerScore++;
-            return `${messageWin} Your score is ${playerScore}`;
-
-        } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
-            playerScore++;
-            return `${messageWin} Your score is ${playerScore}`;
-
-        } else if (playerSelection == computerSelection) {
-            return `${messageTie} Your score is ${playerScore}`;
-
-        }   else {
-            computerScore++;
-            return `${messageLose} Your score is ${playerScore}`;
-        }
-      }
-
-    // Genera 5 partidas
-    for (let i = 0; i < 5; i++) {
-        //let playerSelection = prompt("What is your choice? Rock, Paper or Scissors?", "")
-        let playerSelection = "Rock";
-        const computerSelection = getComputerChoice();
-
-        console.log(`You played ${playerSelection} vs ${computerSelection}. ${playRound(playerSelection, computerSelection)}`);
-
-     }
-
-    console.log(`Your final score is ${playerScore} and the computer score is ${computerScore}`)
-
-    // Resultado final de la partida
-    let finalResult = 
-    (playerScore > computerScore) ? console.log("You are the winner of the game")
-    : (playerScore < computerScore) ? console.log("You lose the game against the computer")
-    : console.log("Nor you or the computer won. Try again");
-};
 
 game();
