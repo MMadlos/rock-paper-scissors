02ac9a9d44c2caf58cbe3a0cb26d39ecf8a62b3f
michael <michaelmadlos@hotmail.com>
2022-10-04 10:49:55 +0200

    Add scores and display it in html

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ function playRound(playerSelection, computerSelection) {
     
     for(i = 0; i < conditionsToWin.length; i++){  
         if (conditionsToWin[i].player == playerSelection && conditionsToWin[i].computer == computerSelection) {
-            return console.log(`You played ${playerSelection} and the computer played ${computerSelection}`)
+            return result = "win";
 
         } else if (playerSelection == computerSelection){
-            return console.log(`You played ${playerSelection} and the computer played ${computerSelection}`)  
+            return result = "tie";
 
         } else {
-            return console.log(`You played ${playerSelection} and the computer played ${computerSelection}`) 
+            return result = "lose";
 
         }; 
     }  
@@ -35,17 +35,16 @@ function game() {
             let playerSelection = button.textContent;     
             let computerSelection = getComputerSelection(); 
 
-            playRound(playerSelection, computerSelection);
-            addCounter();
-            
+            playRound(playerSelection, computerSelection);    
+            addScores();
+            addRoundCounter();
         })
     })
 }
 
 game();
 
-
-function addCounter() {
+function addRoundCounter() {
     let roundNumber = document.getElementById("roundNumber");
     let roundCounter = Number(roundNumber.textContent);
 
@@ -53,7 +52,41 @@ function addCounter() {
         roundCounter++;
         roundNumber.textContent = roundCounter;            
         console.log(roundCounter);
+    } else {
+    console.log("Ha superado las 5 partidas");
     }
-    return console.log("Ha superado las 5 paridas");
 }
 
+// Add scores
+
+function addScores(){
+    let playerNumber = document.getElementById("playerNumber");
+    let computerNumber = document.getElementById("computerNumber");
+    
+    let playerScore = Number(playerNumber.textContent);
+    let computerScore = Number(computerNumber.textContent);
+
+    if(result == "win"){
+        playerScore++
+        playerNumber.textContent = playerScore;
+    } else if (result == "lose"){
+        computerScore++
+        computerNumber.textContent = computerScore;
+    } else {
+        console.log("it's a tie");
+    }
+}
+
+// Add winner of 5
+// Cuando el contador de rondas llegue a 5
+if(roundCounter == 5){}
+// Evalúa quién tiene más puntos
+if(playerScore > computerScore){
+    console.log("You WIN!")
+} else if (playerScore < computerScore) {
+    console.log("You lose...");
+} else {
+    console.log("Tie")
+}
+
+// Si no llega a 5, permite seguir jugando
